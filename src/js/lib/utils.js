5d24e9ef98d96a9866f648b0e611328801a0332e
Danny Amirault <dana@microsoft.com>
2023-02-15 12:05:39 -0500

    Show minutes when meetings are closer than 5 hours

--- src/js/lib/utils.js
+++ src/js/lib/utils.js
@@ -43,7 +43,7 @@ export function sec2time (timeInSeconds) {
   const minutes = Math.floor(time / 60) % 60
   const seconds = Math.floor(time - minutes * 60)
 
-  if (hours > 1) return `${sign}${hours}h`
+  if (hours > 4) return `~${sign}${hours}h`
   if (hours > 0 && minutes === 0) return `${sign}${hours}h 0m`
   if (hours > 0) return `${sign}${hours}h ${minutes}m`
   if (minutes > 0) return `${sign}${minutes}m`
@@ -94,8 +94,10 @@ export function trimForMarquee (text, width, start = 0, lines = 1) {
   if (lines > 1) {
     let lineStart = start + width
     for (let i = 1; i < lines; i++) {
-      segment = segment.concat('\n', text.slice(lineStart, (lineStart + width)))
-      lineStart = lineStart + width
+      if (lineStart < text.length){
+        segment = segment.concat('\n', text.slice(lineStart, (lineStart + width)))
+        lineStart = lineStart + width
+      }
     }
   }
 
